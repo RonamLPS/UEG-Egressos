cd336b51589424b3580e99383c1f19de70a1a3e9
RonamLPS <126636457+RonamLPS@users.noreply.github.com>
2023-10-16 21:14:26 -0300

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,11 @@
+// Parallax Área
+let = document.getElementById("");
+let = document.getElementById("");
+let = document.getElementById("");
+let = document.getElementById("");
+let = document.getElementById("");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+});
+// Fim Parallax Área
